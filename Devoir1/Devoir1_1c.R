d31ea79ecb09bcf9721b023b5740abcc38d8e66b
TaxDh <dhmo123@hotmail.com>
2024-02-06 21:34:05 -0500

    devoir1

--- Devoir1/Devoir1_1c.R
+++ Devoir1/Devoir1_1c.R
@@ -1,4 +1,6 @@
-#fonction ti_discrete page 5/44 de 02-GenerationLoisConnus
+set.seed(10)
+
+#fonction ti_discrete page 5/44 qui a été modifié en t_inverse (seul le nom a été changé...)
 t_inverse <- function(n, x, p){
   
   if(length(x) != length(p)) stop("Dimensions de x et p incompatibles")
@@ -34,15 +36,53 @@ for (m in 1:10) {
   times_croissante[m] <- end_time - start_time
 }
 
+
 # Méthode décroissante
+x_decroissante <- rep(0, length(x))
+p_decroissante <- rep(0, length(p))
+
+for(i in 1:length(x)){
+  x_decroissante[i] <- x[length(x) - i + 1]
+  p_decroissante[i] <- p[length(p) - i + 1]
+  i <- i+1
+}
+
+
 for (m in 1:10) {
   start_time <- Sys.time()
-  ech <- t_inverse(5000, x, p)
+  ech <- t_inverse(5000, x_decroissante, p)
   end_time <- Sys.time()
   times_decroissante[m] <- end_time - start_time
 }
 
 # Méthode optimale
+x_optimale <- x
+p_optimale <- p
+
+for (i in 1:(length(p)-1)) {
+  max_idx <- i
+  for (j in (i+1):length(p)) {
+    if (p_optimale[j] > p_optimale[max_idx]) {
+      max_idx <- j
+    }
+  }
+  
+  # Si l'élément maximal n'est pas déjà à sa place, échanger les éléments
+  if (max_idx != i) {
+    # Échanger les p
+    temp_p <- p_optimale[i]
+    p_optimale[i] <- p_optimale[max_idx]
+    p_optimale[max_idx] <- temp_p
+    
+    # Échanger les x
+    temp_x <- x_optimale[i]
+    x_optimale[i] <- x_optimale[max_idx]
+    x_optimale[max_idx] <- temp_x
+  }
+}
+
+
+
 for (m in 1:10) {
   start_time <- Sys.time()
   ech <- t_inverse(5000, x, p)
@@ -50,10 +90,7 @@ for (m in 1:10) {
   times_optimale[m] <- end_time - start_time
 }
 
-# Faites de même pour les méthodes décroissante et optimale
-
-# Puis créez des histogrammes pour chaque set de temps
-# Vous pouvez utiliser la fonction par() pour afficher plusieurs graphiques sur la même fenêtre graphique si vous le souhaitez
+#voici les 3 histogrammes pour comparé les 3 méthodes
 hist(times_croissante, probability = TRUE, main="Méthode Croissante")
 hist(times_decroissante, probability = TRUE, main="Méthode Décroissante")
 hist(times_optimale, probability = TRUE, main="Méthode Optimale")
